3932a2680e7e4dbe0b5cbe936635ee91fc512588
pool1892 <pool1892@users.noreply.github.com>
2017-09-11 23:00:06 +0200

    Update ssd_pascal.py

--- examples/ssd/ssd_pascal.py
+++ examples/ssd/ssd_pascal.py
@@ -263,7 +263,7 @@ pretrain_model = "models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel"
 label_map_file = "data/VOC0712/labelmap_voc.prototxt"
 
 # MultiBoxLoss parameters.
-num_classes = 21
+num_classes = 2
 share_location = True
 background_label_id=0
 train_on_diff_gt = True
